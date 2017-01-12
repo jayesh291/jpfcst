11124c0bef73b1d7eb330b444835cb5878b8800a
Dnyaneshwar Dewadkar <dewadkar@gmail.com>
2017-01-12 15:37:09 +0530

    Sliding window function added.

--- main.R
+++ main.R
@@ -10,7 +10,7 @@ source("ddfcst/plots.R")
 # setwd("Desktop/ikl/delmon/git/jpfcst/")
 # meterdata <- trained_data_set("temp_dmd_data_daily.txt")
 # str(meterdata)
-# plot_daily_meter_consumption(meterdata,daily_dmd_plot_filename)
+ # plot_daily_meter_consumption(meterdata,daily_dmd_plot_filename)
 # timeseriesData <- preprocess(meterdata)
 # distanceMatrix <- distanceMatrixCorr(timeseriesData)
 # numOfClust = 15
@@ -18,8 +18,17 @@ source("ddfcst/plots.R")
 # # timeseriesHclustUsingCorrelation(timeseriesData)
 # head(timeseriesData)
 # representiveTimeSeries <- representativeTimeSeries(tsHclust,timeseriesData,meterdata)
+# head(representiveTimeSeries)
+# #of clusters equals representativetimeseries
+# str(unique(meterdata$id))
 
-
-
+  # windowSize=7
+  # stepSize=1
+  # i = 1
+  # meterid = "52D61DBC-595E-4E10-BA46-9EF4EE0650C2"
+  # mtr.id.data = meterdata[meterdata$id == meterid & meterdata$ts1 > "2016-11-26",]
+  # result <- getSlidingWindows(mtr.id.data, windowSize,i)
+  # print(result)
+ 
 
 
