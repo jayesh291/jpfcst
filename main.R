fe2c8f91d5a9dc53c5ee7f22371fd97ccfae1e29
Dnyaneshwar Dewadkar <dewadkar@gmail.com>
2017-01-12 13:31:40 +0530

    Updated till 12th Jan - Representative series(s) plotting.

---
+++ main.R
@@ -0,0 +1,25 @@
+
+
+source("ddfcst/libs.R")
+source("ddfcst/constants.R")
+source("ddfcst/dataset.R")
+source("ddfcst/preprocess.R")
+source("ddfcst/engine.R")
+source("ddfcst/timeseriesProcessing.R")
+source("ddfcst/plots.R")
+# setwd("Desktop/ikl/delmon/git/jpfcst/")
+# meterdata <- trained_data_set("temp_dmd_data_daily.txt")
+# str(meterdata)
+# plot_daily_meter_consumption(meterdata,daily_dmd_plot_filename)
+# timeseriesData <- preprocess(meterdata)
+# distanceMatrix <- distanceMatrixCorr(timeseriesData)
+# numOfClust = 15
+# tsHclust <- tsHclust(distanceMatrix, numOfClust = numOfClust)
+# # timeseriesHclustUsingCorrelation(timeseriesData)
+# head(timeseriesData)
+# representiveTimeSeries <- representativeTimeSeries(tsHclust,timeseriesData,meterdata)
+
+
+
+
+
