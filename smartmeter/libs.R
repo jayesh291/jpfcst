4c758f89cffff170cf962ff3efd1100aa684a62e
Dnyaneshwar Dewadkar <dewadkar@gmail.com>
2017-02-09 13:20:14 +0530

    Auto package installer script updated.

--- smartmeter/libs.R
+++ smartmeter/libs.R
@@ -47,4 +47,19 @@ library(forecast)
 # library(dplyr)
 # library(RColorBrewer)
 
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+# usage
+packages <- c("ggplot2", "plyr", "reshape2", "RColorBrewer", "scales", "grid")
+packages <- c(packages,"data.table","psych","plotrix","corrplot","dtw","zoo","proxy")
+packages <- c(packages,"forecast")
+
+ipak(packages)
+
+
+
 
