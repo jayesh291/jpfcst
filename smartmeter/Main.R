c00a96d88576b1864ee5de2eb0c4d8d9b6dad891
Dnyaneshwar Dewadkar <dewadkar@gmail.com>
2017-02-10 15:55:54 +0530

    Decoposition of TS data plot with Forecast added for visualization.

--- smartmeter/Main.R
+++ smartmeter/Main.R
@@ -1,11 +1,20 @@
-source("constants.R")
-source("dataset.R")
+
 source("customizedForecast.R")
 
 meterdata <- trained_data_set("dmd_data_daily_170112.txt")
 meterids <- unique(meterdata$id)
 meterid <- sample(meterids,1)
-singleMeterData <- meterdata[meterdata$id == meterid,]
-forecastikl(singleMeterData$val,7)
-fc <- forecastikl(singleMeterData$val,7)
-ggplot(as.data.frame(fc), aes(c(1:length(fc$tsMeterData))))+geom_line(aes(y = fc$rdpwm, colour = "Predicted"))+geom_line(aes(y = fc$tsMeterData, colour = "Actual"))
+pdf(file="./outs/fctplot_10_FEB_v.pdf")
+
+for(meterid in meterids){
+ singleMeterData <- meterdata[meterdata$id == meterid,]
+ singleMeterData[is.na(singleMeterData)] <- 0
+ forecastikl(singleMeterData$val,7)
+ fc <- forecastikl(singleMeterData$val,7)
+ plot(0,0,xlim = c(1,length(fc$tsMeterData)),ylim = c(min(singleMeterData$val),max(singleMeterData$val)),type = "n",xlab = meterid)
+ lines(fc$tsMeterData,type = 'l')
+ lines(fc$rdpwm,type = 'l', col = "red")
+ dec <- decompose(ts(fc$tsMeterData, frequency = 7))
+ plot(dec)
+}
+dev.off()
