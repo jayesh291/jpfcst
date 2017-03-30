09dcf047d12a56abd6aabf61ad88f9e7ce0e6160
Dnyaneshwar Dewadkar <dewadkar@gmail.com>
2017-03-30 14:31:50 +0530

    Updates for to return only multiplier

--- smartmeter/dailyPattern.R
+++ smartmeter/dailyPattern.R
@@ -1,11 +1,11 @@
-dailyPattern <- function(orgData,movingAverage,noOfDays){
+dailyPattern <- function(movingAverage,noOfDays){
   dpwma <- c()
   i=1
   weightCount=1
   prevWeek <- 2*noOfDays
   for(element in movingAverage){
     if( i > prevWeek){
-      dpwma[i] <- movingAverage[i-noOfDays]* orgData[i - noOfDays]/movingAverage[i - prevWeek]  
+      dpwma[i] <- movingAverage[i-noOfDays]/movingAverage[i - prevWeek]  
     }else{
       dpwma[i] <- movingAverage[i]
     }
