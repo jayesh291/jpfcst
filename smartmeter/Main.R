672c2ca8bb560108477f0a0376cfec94bdcce5f0
Dnyaneshwar Dewadkar <dewadkar@gmail.com>
2017-03-30 14:33:26 +0530

    Refactored for multiplier.

--- smartmeter/Main.R
+++ smartmeter/Main.R
@@ -1,12 +1,13 @@
 rm(list = ls())
 
 
+
 getwd()
 setwd("smartmeter/")
 source("libs.R")
 source("dataset.R")
+source("basevalue.R")
 source("movingAverage.R")
-source("weightedMovingAverage.R")
 source("dailyPattern.R")
 source("ratioPrevMA.R")
 library(gdata)
@@ -19,37 +20,43 @@ pdf(file=paste0("./outs/fctplot",todaysDate,".pdf"))
 errorSummary <- c()
 noOfDaystoPredict <- 7
 for(meterid in meterids){
+  
+  
   singleMeterData <- meterdata[meterdata$id == meterid,]
   singleMeterData[is.na(singleMeterData)] <- 0
   tsMeterData <- singleMeterData$val
-  # write.csv(singleMeterData,file = "./outs/meterdata.csv")
   ma <- movingAverage(tsMeterData,noOfDaystoPredict)
   singleMeterData$ma <- ma
-  dailyPatterns <- dailyPattern(tsMeterData,ma,noOfDaystoPredict)
+  baseValue <- basevalue(tsMeterData,noOfDaystoPredict)
+  singleMeterData$baseValue <- baseValue
+  dailyPatterns <- dailyPattern(ma,noOfDaystoPredict)
   singleMeterData$dailyPattern <- dailyPatterns
-  rdpwm <- ratioPrevMA(ma,dailyPatterns, noOfDaystoPredict)
-  singleMeterData$trend <- rdpwm
-  write.csv(singleMeterData,file = "./outs/meterdata_ma_dp_trend_v2.csv")
-  forecastData <- cbind(tsMeterData,ma,dailyPatterns,rdpwm)
+  trend <- ratioPrevMA(ma,dailyPatterns, noOfDaystoPredict)
+  singleMeterData$trend <- trend
+  prediction <- baseValue*dailyPatterns*trend
+  
+  singleMeterData$pred <- prediction
+  # write.csv(singleMeterData,file = paste0("./outs/",meterid,".csv"))
+  forecastData <- cbind(tsMeterData,prediction)
   fc <- as.data.frame(forecastData)
 #  Plot the graph with actual and predicted
   plot(0,0,xlim = c(1,length(fc$tsMeterData)),ylim = c(min(singleMeterData$val),max(singleMeterData$val)),type = "n",xlab = meterid)
   lines(fc$tsMeterData,type = 'l')
-  lines(singleMeterData$val,type = 'l',col="blue")
-  lines(fc$rdpwm,type = 'l', col = "red")
+  lines(singleMeterData$val[1:(length(tsMeterData)-noOfDaystoPredict)],type = 'l',col="blue")
+  lines(fc$prediction,type = 'l', col = "red")
   # Forecast error calulation 
   # MAPE
-  forecastError <- fc$tsMeterData[1:(length(fc$rdpwm)-8)]-fc$rdpwm[1:(length(fc$rdpwm)-8)]
-  mapeForecastError <- abs(forecastError/fc$tsMeterData[1:(length(rdpwm)-8)]) * 100
+  forecastError <- fc$tsMeterData[1:(length(fc$prediction)-8)]-fc$prediction[1:(length(fc$prediction)-8)]
+  mapeForecastError <- abs(forecastError/fc$tsMeterData[1:(length(trend)-8)]) * 100
   mapeForecastError[is.na(mapeForecastError)] <- 0.00001
   mapeForecastError[is.infinite(mapeForecastError)] <- 0.00001
   # sMAPE
-  denominator <- abs(fc$rdpwm[1:(length(fc$rdpwm)-8)]+fc$tsMeterData[1:(length(fc$rdpwm)-8)])/2
-  nmratr <- abs(fc$rdpwm[1:(length(fc$rdpwm)-8)]-fc$tsMeterData[1:(length(fc$rdpwm)-8)])
+  denominator <- abs(fc$prediction[1:(length(fc$prediction)-8)]+fc$tsMeterData[1:(length(fc$prediction)-8)])/2
+  nmratr <- abs(fc$prediction[1:(length(fc$prediction)-8)]-fc$tsMeterData[1:(length(fc$prediction)-8)])
   smape = (1/7)* nmratr /denominator;
-  datas <- cbindX(data.frame(fc$tsMeterData), data.frame(fc$rdpwm), data.frame(mapeForecastError),data.frame(smape))
+  datas <- cbindX(data.frame(fc$tsMeterData), data.frame(fc$prediction), data.frame(mapeForecastError),data.frame(smape))
   df.fc <- as.data.frame(datas)
-  # Forecast error : Average of MAPE 
+  # Forecast error : Average of MAPE
   mapeMean=mean(mapeForecastError[length(mapeForecastError)-15:(length(mapeForecastError)-8)])
   errorSummary <- c(errorSummary,mapeMean)
   fileName <- paste0("./outs/",meterid,"mape_",mapeMean,"_",todaysDate,".csv")
