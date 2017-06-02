77f16102bba55b8b37d5040fdcfe273742f4dc0b
Dnyaneshwar Dewadkar <dewadkar@gmail.com>
2017-06-02 13:36:07 +0530

    fixed bug of missing date imputation

--- smartmeter/datesUtil.R
+++ smartmeter/datesUtil.R
@@ -1,14 +1,16 @@
 
 missingDates <- function(data,dateCol) {
+  # data <- originalData
+  # dateCol <- "ts1"
   totalDiffDays <- abs(data[dateCol][1,] - data[nrow(data),c(dateCol)])
   missingDays <- totalDiffDays - nrow(data)+1
   message("Missing days in timeseries are : ",missingDays)
-  alldays <- seq(data[1,dateCol],length=(totalDiffDays+missingDays-1),by="+1 day")
+  alldays <- seq(data[1,dateCol],length=(totalDiffDays+1),by="+1 day")
   missingDates = alldays[!(alldays %in% data[,dateCol])]
   return(missingDates)
 }
 
-
+# missingDates(singleMeterData,"ts1")
 
 addMissingDates <- function(data,dateCol) {
   
@@ -17,7 +19,7 @@ addMissingDates <- function(data,dateCol) {
   # Append this `data.frame` and resort:
   i <- 1
   while(i <= length(missingDates)){
-    message(" i loopings -> ",i, " -- ",as.POSIXct(missingDates[i]))
+    # message(" i loopings -> ",i, " -- ",as.POSIXct(missingDates[i]))
     tempDate <- format(as.POSIXct(missingDates[i], origin="1970-01-01"),"%Y-%m-%d")
     data = rbind(data, c(as.character(meterid),as.character(tempDate),NA,tempDate),stringsAsFactors = FALSE)
     i <- i+1
