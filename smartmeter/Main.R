d574fa7ac6f14cfea35038777330fcd74dfc4802
Dnyaneshwar Dewadkar <dewadkar@gmail.com>
2017-06-22 15:30:51 +0530

    Reverting predicted value outlier detection.

--- smartmeter/Main.R
+++ smartmeter/Main.R
@@ -16,6 +16,17 @@ source("timeseriesImpute.R")
 meterdata <- trained_data_set("./inputs/daily_dmd_data_20170517.txt")
 meterids <- unique(meterdata$id)
 length(meterids)
+# exceptional ids
+filter_ids <- c("25e88ffc-cdd0-410d-a212-7b1ddf5fe041","2fb9f4c6-cab3-4a81-b16d-94f16aeada35",
+                "46b801c9-eac3-46de-bc7c-9b0f0f600d9f","a51a414b-d84d-4201-b60c-167d16ce55b2",
+                "0071cfb0-d92d-4035-aba6-1ab961e4f573","0114170c-47be-4b74-ae94-b90f39f8fc99",
+                "47f10256-12ab-4aa3-af21-d490a54d403d")
+which(meterids %in% c("a51a414b-d84d-4201-b60c-167d16ce55b2"))
+
+meterids <- subset(meterids, !(meterids %in% c(filter_ids)))
+
+length(meterids)
+# meterids <- meterids[1:length(meterids)]
 
 todaysDate <-format(Sys.time(), "%a%b%d%Y%H%S")
 pdf(file=paste0("./outs/meters_",todaysDate,"_plot.pdf"))
@@ -29,8 +40,8 @@ cyclePeriod <- 7
 longPrediction = 3
 
 numOfMetersToProcess = length(meterids)
-numOfMetersToProcess=10
-numOfDaysToForecast = 20
+numOfMetersToProcess=30
+numOfDaysToForecast = 90
 
 error_summary <- c("id","mape")
 
@@ -38,7 +49,7 @@ error_summary <- c("id","mape")
 for(i in 1:numOfMetersToProcess){
   
   # Random pick one id from list
-  meterid <- sample(meterids,1)
+  meterid <- meterids[i]
   message("Processing meter id - ",meterid)
   
   singleMeterData <- meterdata[meterdata$id == meterid,]
@@ -49,9 +60,9 @@ for(i in 1:numOfMetersToProcess){
   
   originalData <- singleMeterData
 
-  # Outlier detection and replacements: Detect the outliers and remove those rows. Consider 
+  # Outlier detection and replacements: Detect the outliers and remove those rows. Consider
   # it as missing information.
-  outlierTSO <- tso(ts(singleMeterData$val),delta = 0.7,types = c("LS"))
+  try(outlierTSO <- tso(ts(singleMeterData$val),delta = 0.7,types = c("LS")))
   # nrow(outlierTSO$outliers)
   message("For ",meterid,", # of rows ",nrow(outlierTSO$outliers))
   if(nrow(outlierTSO$outliers) > 0){
@@ -68,6 +79,8 @@ for(i in 1:numOfMetersToProcess){
   if(TRUE %in% indx){
     message(" Please check missing data !!! ")
   }
+  
+  originalData.imputed <- singleMeterData$val
   
   countr = 0;
   while(countr < numOfDaysToForecast){
@@ -120,29 +133,39 @@ for(i in 1:numOfMetersToProcess){
     fc <- fc[-(nrow(fc)-1),]
     singleMeterData <- fc
   }
+  
+  numOfDays = length(originalData.imputed)
+  ignoreTop = ceiling(numOfDays*30/100)
+  
+  singleMeterData$prediction[is.na(singleMeterData$prediction)] <- 0
+  singleMeterData$prediction[is.nan(singleMeterData$prediction)] <- 0
+  singleMeterData$prediction[is.infinite(singleMeterData$prediction)] <- 0
+  
   message(" y limits ",c(min(singleMeterData$prediction,na.rm = TRUE),max(singleMeterData$prediction, na.rm = TRUE)))
   ylimMin <- 0
   if( !is.infinite(min(singleMeterData$prediction,na.rm = TRUE))){
-    ylimMin <- min(singleMeterData$prediction,na.rm = TRUE)
+    ylimMin <- min(singleMeterData$prediction[ignoreTop:length(singleMeterData$prediction)],na.rm = TRUE)
   }
   ylimMax <- 1
   if(!is.infinite(max(singleMeterData$prediction, na.rm = TRUE))){
-    ylimMax <- max(singleMeterData$prediction, na.rm = TRUE)
+    ylimMax <- max(singleMeterData$prediction[ignoreTop:length(singleMeterData$prediction)], na.rm = TRUE)
   }
   
   write.csv(fc,file = paste0("./outs/",meterid,"_",todaysDate,".csv"))
   try(plot(0,0,xlim = c(1,length(tsMeterData)),ylim = c(ylimMin,ylimMax),type = "n",xlab = meterid))
   historicalData <- meterdata[meterdata$id == meterid,]
-  lines(historicalData$val,type = 'l',col="blue")
-  lines(singleMeterData$prediction,type = 'l', col = "red")
-  lines(singleMeterData$basePrediction,type = 'l', col = "green")
+  lines(historicalData$val[ignoreTop:length(historicalData$val)],type = 'l',col="blue")
+  lines(singleMeterData$prediction[ignoreTop:length(singleMeterData$prediction)],type = 'l', col = "red")
+  # lines(singleMeterData$basePrediction,type = 'l', col = "green")
   
-  abs_error <- abs(as.numeric(singleMeterData$val) - as.numeric(singleMeterData$prediction))/(as.numeric(singleMeterData$val))
-  mape <- sum(abs_error,na.rm = TRUE)*100/nrow(singleMeterData)
+  
+  abs_error <- abs(as.numeric(singleMeterData$val[ignoreTop:numOfDays]) - as.numeric(singleMeterData$prediction[ignoreTop:numOfDays]))/(as.numeric(singleMeterData$val[ignoreTop:numOfDays]))
+  mape <- sum(abs_error,na.rm = TRUE)*100/numOfDays
   mape
   error_summary <- rbind(error_summary,c(as.character(meterid),as.numeric(mape)))
 }
 
 write.csv(file = paste0("./outs/error_summary",todaysDate,".csv"),error_summary)
 dev.off()
-graphics.off()
+# graphics.off()
+
