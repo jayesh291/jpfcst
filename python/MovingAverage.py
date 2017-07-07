30265f8118b95233c2022bfb29cfacb55aa4a6ba
Dnyaneshwar Dewadkar <dewadkar@gmail.com>
2017-07-07 16:20:53 +0530

    Added individual test functionality for the funtion in the file.

--- python/MovingAverage.py
+++ python/MovingAverage.py
@@ -35,23 +35,11 @@ meterdata = trainingDataSet('dmd_data_daily_170112.txt')
 singleMeterData = meterdata[meterdata['id'] == "0071CFB0-D92D-4035-ABA6-1AB961E4F573"]
 sortedSingleMeterData = singleMeterData.sort_values(["ts", "id", "val"], ascending=[1, 0, 0])
 timeseries = sortedSingleMeterData.val
-
-# dfm.index = range(1,len(dfm) + 1)
 sortedSingleMeterData.index = range(1, len(sortedSingleMeterData) + 1)
 data = sortedSingleMeterData.to_csv()
-# save_data(data,fileName="singleMeterData.csv")
-# df = sortedSingleMeterData.set_index(np.(len(sortedSingleMeterData.index)))
-
-# df2.set_index(np.arange(len(df2.index)))
-
-
 ma = movingAverage(sortedSingleMeterData.val, 7)
-
-import pickle
-
+# write to a file to verify the moving average
 with open('outfile1.csv', 'w') as fp:
     fp.write("\n".join(map(lambda x: str(x), ma)))
     fp.close()
 
-    # fp.write(ma)
-    # .join([str(i) for i in value_list])
