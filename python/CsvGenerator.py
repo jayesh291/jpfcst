e5e6dbc1da63e73549fdb2f91b09a87806b8d8ea
Dnyaneshwar Dewadkar <dewadkar@gmail.com>
2017-07-24 18:07:11 +0530

    Append the output for analysis in a file.

--- python/CsvGenerator.py
+++ python/CsvGenerator.py
@@ -6,10 +6,10 @@ import pandas as pd
 def generator_csv(file_name,data):
     now = datetime.now()
     val = "%s%s%s%s%s%s" % (now.year, now.month, now.day, now.hour, now.month, now.second)
-    file_name = file_name + val + ".csv"
+    file_name += val + ".csv"
     df=pd.DataFrame(data)
     csv=df.to_csv(index=False)
-    with open(file_name,'wb') as file:
+    with open(file_name,'w') as file:
         file.write(csv)
     file.close()
 
